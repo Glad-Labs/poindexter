184a90885b126de8d88f7cd004f2be069b450b24
mattyglads <92560039+mattyglads@users.noreply.github.com>
2025-10-16 23:20:14 -0400

    fix: add encryption key and force postgres database client

--- cms/strapi-v5-backend/config/server.ts
+++ cms/strapi-v5-backend/config/server.ts
@@ -9,12 +9,4 @@ export default ({ env }) => ({
     'PUBLIC_URL',
     'https://glad-labs-strapi-v5-backend-production.up.railway.app'
   ), // Public URL for admin panel
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-      options: {
-        expiresIn: '7d',
-      },
-    },
-  },
 });
