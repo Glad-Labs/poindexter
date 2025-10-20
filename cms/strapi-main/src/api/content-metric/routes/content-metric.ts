2754fe379791048021cf0a608e5f0aecdd91b840
mattyglads <92560039+mattyglads@users.noreply.github.com>
2025-10-19 22:13:13 -0400

    feat: add content types, controllers, routes, and services for about, author, category, content-metric, post, privacy-policy, and tag

---
+++ cms/strapi-main/src/api/content-metric/routes/content-metric.ts
@@ -0,0 +1,7 @@
+/**
+ * content-metric router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::content-metric.content-metric');
---
+++ cms/strapi-main/src/api/content-metric/services/content-metric.ts
@@ -0,0 +1,9 @@
+/**
+ * content-metric service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService(
+  'api::content-metric.content-metric'
+);
